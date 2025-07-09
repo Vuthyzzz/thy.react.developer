b94f73a35f43c1ae697debbd7858179fa7f48ef7
Vuthyzzz <nopvuthy027@gmail.com>
2025-07-09 13:03:15 +0700

    Update: fix routing and image paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import './App.css';
 import Header from './Header';
 
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './Home';
 import Contact from './Contact';
